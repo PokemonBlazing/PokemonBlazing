231f409a43ace48248f0fbb0a307f6bef1f8f192
Eze Zito <ezezitoo@gmail.com>
2023-09-20 16:43:44 -0300

    Inicio servidor

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+require('ejs');
+
+const app = express();
+
+// REQUERIMIENTO ARCHIVOS EXTERNOS
+const MainRoutes = require('./routes/main-routes');
+
+// SETTINGS
+
+app.set('viewengine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+// MIDDLEWARES
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(session({
+    secret: 'mysecretkey_pokemon',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+app.use(cookieParser('mysecretkey_pokemon'))
+
+// RUTAS
+
+app.use(MainRoutes);
+
+// INICIO SERVIDOR
+
+app.listen(3000);
+console.log("Servidor escuchando en http://localhost:3000");
