4ae0efb0905660d7f5f74a7449d0e8ff977f1dde
Tuken04 <109147964+Tuken04@users.noreply.github.com>
2023-10-05 14:54:33 -0300

    Funcionamiento parcial peleas
    Co-Authored-By: zitongo <ezezitoo@gmail.com>

--- src/controllers/encounters.js
+++ src/controllers/encounters.js
@@ -20,6 +20,7 @@ let pokemon  =[
             pp: 25,
             accuracy: 100
         },
+        HPmax: 45,
         HP: 45,
         deff: 49,
         atk:49,
@@ -67,58 +68,58 @@ let pokemon  =[
 function ataque1(p1,p2) {
     //Daño planta -> fuego (poco eficaz)
     if(p1.type==="grass" && p2.type==="fire"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño planta -> agua (muy eficaz)
     if(p1.type==="grass" && p2.type==="water"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
     //Daño fuego -> planta (muy eficaz)
     if(p1.type==="fire" && p2.type==="grass"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
     //Daño fuego -> agua (poco eficaz)
     if(p1.type==="fire" && p2.type==="water"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño agua -> planta (poco eficaz)
     if(p1.type==="water" && p2.type==="grass"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño agua -> fuego (muy eficaz)
     if(p1.type==="water" && p2.type==="fire"){
-        p2.HP = p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move1.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
 
 }
 function ataque2(p1,p2) {
     //Daño planta -> fuego (poco eficaz)
     if(p1.type==="grass" && p2.type==="fire"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño planta -> agua (muy eficaz)
     if(p1.type==="grass" && p2.type==="water"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
     //Daño fuego -> planta (muy eficaz)
     if(p1.type==="fire" && p2.type==="grass"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
     //Daño fuego -> agua (poco eficaz)
     if(p1.type==="fire" && p2.type==="water"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño agua -> planta (poco eficaz)
     if(p1.type==="water" && p2.type==="grass"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 0.5)
     }
     //Daño agua -> fuego (muy eficaz)
     if(p1.type==="water" && p2.type==="fire"){
-        p2.HP = p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5
+        p2.HP = Math.ceil(p2.HP - (2 * p1.move2.power * (p1.atk/p2.deff)/50+2) * 1.5)
     }
 }
 function curar(p1){
-    p1.HP += 45;
+    p1.HP = p1.HPmax;
 }
 module.exports = 
 {pokemon, ataque1, ataque2, curar} 
